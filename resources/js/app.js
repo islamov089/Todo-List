6d0ceff047b72ccff156c644c2cbdaa80c2db0dd
Ilhom <Ilhom.Islomov@inoviagroup.se>
2024-08-19 01:00:24 +0500

    Add styles to UI

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,5 +1,16 @@
 import { createApp } from 'vue';
 import App from './vue/app.vue';
 
+import { library } from '@fortawesome/fontawesome-svg-core'; // Import only once
+import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome';
+
+import { faPlusSquare, faTrash } from '@fortawesome/free-solid-svg-icons';
+
+library.add(faPlusSquare, faTrash);
+
 const app = createApp(App);
+
+app.component('font-awesome-icon', FontAwesomeIcon);
+
+
 app.mount('#app');
--- resources/js/vue/app.vue
+++ resources/js/vue/app.vue
@@ -1,11 +1,51 @@
 <template>
-    <div>
-      <h1>Todo List app</h1>
+    <div class="todoListContainer">
+      <div class="heading">
+        <h2 id="title">Todo List</h2>
+        <add-item-form />
+      </div>
+      <list-view />
     </div>
   </template>
   
   <script>
+  import addItemForm from './addItemForm.vue';
+  import listView from './listView.vue';
+  
   export default {
-    name: 'App',
+    components: {
+      addItemForm,
+      listView,
+    },
   };
   </script>
+  
+  <style scoped>
+  .todoListContainer {
+    width: 550px;
+    margin: auto;
+    background: #f5f5f5; 
+    border-radius: 10px; 
+    box-shadow: 0 4px 10px rgba(0, 0, 0, 0.1);
+  }
+  
+  .heading {
+    background: linear-gradient(135deg, #74ebd5, #9face6); 
+    padding: 20px;
+    border-top-left-radius: 10px;
+    border-top-right-radius: 10px;
+  }
+  
+  #title {
+    text-align: center;
+    font-family: 'Roboto', sans-serif;
+    color: #f5f5f5;
+    letter-spacing: 2px; 
+    text-transform: uppercase; 
+    background-color: #5c6bc0; 
+    border-radius: 8px; 
+}
+
+
+  </style>
+  

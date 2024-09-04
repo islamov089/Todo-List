463bf052c716274b275a12a99247df9865e3c717
Ilhom <Ilhom.Islomov@inoviagroup.se>
2024-09-04 10:25:52 +0500

    Thirs commit

--- resources/js/vue/app.vue
+++ resources/js/vue/app.vue
@@ -34,29 +34,4 @@ export default {
 };
 </script>
 
-<!-- <style scoped>
-.todoListContainer {
-  width: 800px;
-  margin: auto;
-  background: #f5f5f5; 
-  border-radius: 10px; 
-  box-shadow: 0 4px 10px rgba(0, 0, 0, 0.1);
-}
 
-.heading {
-  background: linear-gradient(135deg, #74ebd5, #9face6); 
-  padding: 20px;
-  border-top-left-radius: 10px;
-  border-top-right-radius: 10px;
-}
-
-#title {
-  text-align: center;
-  font-family: 'Roboto', sans-serif;
-  color: #f5f5f5;
-  letter-spacing: 2px; 
-  text-transform: uppercase; 
-  background-color: #5c6bc0; 
-  border-radius: 8px; 
-}
-</style> -->

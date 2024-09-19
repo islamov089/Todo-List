b317a8a998b07860ad4720512893f604618a5c31
Ilhom <Ilhom.Islomov@inoviagroup.se>
2024-09-19 10:25:12 +0500

    Fix import File after review

--- modules/Item/Imports/ItemImports.php
+++ modules/Item/Imports/ItemImports.php
@@ -6,24 +6,23 @@ use Carbon\Carbon;
 use Illuminate\Support\Collection;
 use Maatwebsite\Excel\Concerns\Importable;
 use Maatwebsite\Excel\Concerns\ToCollection;
+use Maatwebsite\Excel\Concerns\WithHeadingRow;
 use Modules\Item\Models\Item;
 
-class ItemImports implements ToCollection
+class ItemImports implements ToCollection, WithHeadingRow
 {
     use Importable;
 
     public function collection(Collection $rows)
     {
-        $rows->shift(); 
-
         foreach ($this->rowGenerator($rows) as $row) {
             Item::updateOrCreate(
-                ['name' => trim($row['name'])], 
+                ['name' => $row['name']], 
                 [
-                    'status' => trim($row['status']),
-                    'deadline' => $this->convertToDate(trim($row['deadline'])),
-                    'completed' => isset($row['completed']) ? (bool)trim($row['completed']) : false,
-                    'completed_at' => isset($row['completed_at']) ? $this->convertToDate(trim($row['completed_at'])) : null,
+                    'status' => $row['status'],
+                    'deadline' => $this->convertToDate($row['deadline']),
+                    'completed' => (bool)$row['completed'], 
+                    'completed_at' => $this->convertToDate($row['completed_at']),
                 ]
             );
         }
@@ -33,11 +32,11 @@ class ItemImports implements ToCollection
     {
         foreach ($rows as $row) {
             yield [
-                'name' => $row[0],
-                'status' => $row[1],
-                'deadline' => $row[2],
-                'completed' => $row[3] ?? null,
-                'completed_at' => $row[4] ?? null,
+                'name' => trim($row['name']), 
+                'status' => trim($row['status']),
+                'deadline' => trim($row['deadline']),
+                'completed' => trim($row['completed']) ?? null, 
+                'completed_at' => trim($row['completed_at']) ?? null, 
             ];
         }
     }
@@ -54,7 +53,7 @@ class ItemImports implements ToCollection
             $date = Carbon::parse($value);
             return $date->format('Y-m-d');
         } catch (\Exception $e) {
-            return null;
+            return null; 
         }
     }
 }
